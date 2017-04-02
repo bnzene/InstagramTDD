ec7bb2c7c4eed9d21bcc432008840644da8a263f
Eleanor Kavanagh-Brown <ekavanaghbrown@gmail.com>
2017-04-02 23:41:03 +0100

    Add username to index and show view for each post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -7,14 +7,14 @@ class PostsController < ApplicationController
   end
 
   def new
-    @post = Post.new
+    @post = current_user.posts.build
   end
 
   def create
-    @post = Post.create(post_params)
+    @post = current_user.posts.build(post_params)
     if @post.save
       flash[:success] = "Your post has been created."
-      redirect_to @post
+      redirect_to posts_path
     else
       flash[:alert] = "Halt, you fiend! You need an image to post here!"
       render :new
